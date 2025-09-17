1e3687fec3267927e5141dfff0266ab6417ea7b6
David <30633005+JewishBanana@users.noreply.github.com>
2025-09-17 03:21:48 -0600

    1.1.0

--- src/main/java/com/github/jewishbanana/playerarmorchangeevent/PlayerArmorChangeEvent.java
+++ src/main/java/com/github/jewishbanana/playerarmorchangeevent/PlayerArmorChangeEvent.java
@@ -6,7 +6,14 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * This event is called when a player changes their armor. This event mimics the paper version and is a substitute for spigot/bukkit compatibility on servers.
+ * <br><br>
+ * <STRONG>Keep in mind that this event is only fired when vanilla mechanics change armor (e.g. inventory clicks, armor breaking, etc.) 
+ * Armor that is changed or modified by third party plugins will not fire this event!</STRONG>
+ */
 public class PlayerArmorChangeEvent extends Event implements Cancellable {
    
     private static final HandlerList handlers = new HandlerList();
@@ -18,25 +25,50 @@ public class PlayerArmorChangeEvent extends Event implements Cancellable {
     private EquipmentSlot slot;
     private Reason reason;
 
-    public PlayerArmorChangeEvent(Player player, EquipmentSlot slot, ItemStack oldItem, ItemStack newItem, Reason reason) {
+    public PlayerArmorChangeEvent(@NotNull Player player, EquipmentSlot slot, @NotNull ItemStack oldItem, @NotNull ItemStack newItem, Reason reason) {
         this.player = player;
         this.slot = slot;
         this.oldItem = oldItem;
         this.newItem = newItem;
         this.reason = reason;
     }
-    public Player getPlayer() {
+    /**
+     * Gets the player that is changing their armor
+     * 
+     * @return The involved player in this event
+     */
+    public @NotNull Player getPlayer() {
         return player;
     }
-    public ItemStack getOldItem() {
+    /**
+     * Gets the item that is currently equipped on the player in the armor slot that is being changed. If no armor is present this will return an AIR itemstack.
+     * 
+     * @return The item currently equipped in the armor slot
+     */
+    public @NotNull ItemStack getOldItem() {
         return oldItem;
     }
-    public ItemStack getNewItem() {
+    /**
+     * Gets the item that is going to be equipped in the players armor slot that is being changed. If no armor is being equipped then this will return an AIR itemstack.
+     * 
+     * @return The item that will be equipped in the slot as a result from this event
+     */
+    public @NotNull ItemStack getNewItem() {
         return newItem;
     }
+    /**
+     * Gets the armor equipment slot that is changing on the player.
+     * 
+     * @return The armor equipment slot being changed by this event
+     */
     public EquipmentSlot getSlot() {
         return slot;
     }
+    /**
+     * Gets the reason that the armor is being changed on the player. This is the vanilla related action that is causing this event.
+     * 
+     * @return The reason the armor is being changed
+     */
     public Reason getReason() {
         return reason;
     }
@@ -55,10 +87,25 @@ public class PlayerArmorChangeEvent extends Event implements Cancellable {
     public static HandlerList getHandlerList() {
         return handlers;
     }
+    /**
+     * Represents a vanilla reason of why a {@link PlayerArmorChangeEvent} is being fired.
+     */
     public enum Reason {
+    	/**
+    	 * Represents an inventory related action such as the player manually placing the item in their armor slot, or shift clicking the armor to equip it.
+    	 */
         INVENTORY_ACTION,
+        /**
+         * Represents a player right clicking an armor piece in their hand to equip it.
+         */
         RIGHT_CLICK,
+        /**
+         * Represents a dispenser block firing and equipping the armor onto the player.
+         */
         DISPENSER,
+        /**
+         * Represents the armor piece breaking as a result of its durability reaching 0.
+         */
         ITEM_BREAK;
     }
 }
--- src/main/java/com/github/jewishbanana/playerarmorchangeevent/PlayerArmorListener.java
+++ src/main/java/com/github/jewishbanana/playerarmorchangeevent/PlayerArmorListener.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
@@ -17,11 +18,13 @@ import org.bukkit.block.data.type.RedstoneWire.Connection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockDispenseArmorEvent;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.inventory.InventoryType.SlotType;
@@ -36,90 +39,149 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.github.jewishbanana.playerarmorchangeevent.PlayerArmorChangeEvent.Reason;
 
 public class PlayerArmorListener implements Listener {
-   
-    private Set<InventoryAction> pickUpActions = new HashSet<>(Arrays.asList(InventoryAction.COLLECT_TO_CURSOR, InventoryAction.PICKUP_ALL, InventoryAction.PICKUP_HALF, InventoryAction.PICKUP_ONE, InventoryAction.PICKUP_SOME, InventoryAction.DROP_ALL_SLOT, InventoryAction.DROP_ONE_SLOT, InventoryAction.MOVE_TO_OTHER_INVENTORY));
-    private Set<InventoryAction> placeActions = new HashSet<>(Arrays.asList(InventoryAction.PLACE_ALL, InventoryAction.PLACE_ONE, InventoryAction.PLACE_SOME));
-    private Set<EquipmentSlot> armorSlots = new HashSet<>(Arrays.asList(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET));
+	
+    private final Set<EquipmentSlot> armorSlots = new HashSet<>(Arrays.asList(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET));
    
     public PlayerArmorListener(JavaPlugin plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onInventoryClick(InventoryClickEvent event) {
-        if (event.isCancelled() || event.getClickedInventory() == null || !(event.getWhoClicked() instanceof Player))
+        if (event.getResult() == Result.DENY || event.getClickedInventory() == null || !(event.getWhoClicked() instanceof Player))
             return;
-        if (event.getAction() == InventoryAction.SWAP_WITH_CURSOR && event.getSlotType() == SlotType.ARMOR) {
-            if (isNotNullOrAir(event.getCurrentItem()) && isNotNullOrAir(event.getCursor())) {
-                PlayerArmorChangeEvent armorEvent = new PlayerArmorChangeEvent((Player) event.getWhoClicked(), event.getCurrentItem().getType().getEquipmentSlot(), event.getCurrentItem(), event.getCursor(), Reason.INVENTORY_ACTION);
+        if (event.getSlotType() == SlotType.ARMOR) {
+        	Player player = (Player) event.getWhoClicked();
+        	switch (event.getAction()) {
+        	case PLACE_ALL:
+        	case PLACE_SOME:
+        	case PLACE_ONE:
+        		PlayerArmorChangeEvent placeEvent = new PlayerArmorChangeEvent(player, getEquipmentSlotFromRawID(event.getSlot()), new ItemStack(Material.AIR), event.getCursor(), Reason.INVENTORY_ACTION);
+                Bukkit.getPluginManager().callEvent(placeEvent);
+                if (placeEvent.isCancelled()) {
+                    event.setResult(Result.DENY);
+                    if (player.getGameMode() == GameMode.CREATIVE) {
+                    	if (player.getInventory().firstEmpty() != -1)
+                    		player.getInventory().addItem(event.getCursor());
+                    	else
+                    		player.setItemOnCursor(event.getCursor());
+                    }
+                }
+                return;
+        	case PICKUP_ALL:
+        	case PICKUP_HALF:
+        	case PICKUP_SOME:
+        	case PICKUP_ONE:
+        		PlayerArmorChangeEvent pickupEvent = new PlayerArmorChangeEvent(player, getEquipmentSlotFromRawID(event.getSlot()), event.getCurrentItem(), new ItemStack(Material.AIR), Reason.INVENTORY_ACTION);
+                Bukkit.getPluginManager().callEvent(pickupEvent);
+                if (pickupEvent.isCancelled()) {
+                    event.setResult(Result.DENY);
+                    if (player.getGameMode() == GameMode.CREATIVE) {
+                    	if (player.getInventory().firstEmpty() != -1)
+                    		player.getInventory().addItem(event.getCursor());
+                    	else
+                    		player.setItemOnCursor(event.getCursor());
+                    }
+                }
+                return;
+        	case SWAP_WITH_CURSOR:
+        		PlayerArmorChangeEvent swapCursorEvent = new PlayerArmorChangeEvent(player, getEquipmentSlotFromRawID(event.getSlot()), event.getCurrentItem(), event.getCursor(), Reason.INVENTORY_ACTION);
+                Bukkit.getPluginManager().callEvent(swapCursorEvent);
+                if (swapCursorEvent.isCancelled()) {
+                    event.setResult(Result.DENY);
+                    if (player.getGameMode() == GameMode.CREATIVE) {
+                    	player.setItemOnCursor(new ItemStack(Material.AIR));
+                    	if (player.getInventory().firstEmpty() != -1)
+                    		player.getInventory().addItem(event.getCursor());
+                    	else
+                    		player.setItemOnCursor(event.getCursor());
+                    }
+                }
+        		return;
+        	case HOTBAR_SWAP:
+        		ItemStack item = player.getInventory().getItem(event.getHotbarButton());
+        		PlayerArmorChangeEvent hotbarEvent = new PlayerArmorChangeEvent(player, getEquipmentSlotFromRawID(event.getSlot()), event.getCurrentItem() != null ? event.getCurrentItem() : new ItemStack(Material.AIR), item != null ? item : new ItemStack(Material.AIR), Reason.INVENTORY_ACTION);
+                Bukkit.getPluginManager().callEvent(hotbarEvent);
+                if (hotbarEvent.isCancelled())
+                    event.setResult(Result.DENY);
+                return;
+        	case MOVE_TO_OTHER_INVENTORY:
+        		PlayerArmorChangeEvent moveToOtherEvent = new PlayerArmorChangeEvent(player, getEquipmentSlotFromRawID(event.getSlot()), event.getCurrentItem(), new ItemStack(Material.AIR), Reason.INVENTORY_ACTION);
+                Bukkit.getPluginManager().callEvent(moveToOtherEvent);
+                if (moveToOtherEvent.isCancelled())
+                    event.setResult(Result.DENY);
+                return;
+            default:
+            	return;
+        	}
+        } else if (event.isShiftClick() && event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY && event.getClickedInventory().getType() == InventoryType.PLAYER && isNotNullOrAir(event.getCurrentItem()) && getTopInventoryType(event) == InventoryType.CRAFTING) {
+        	Player player = (Player) event.getWhoClicked();
+        	EquipmentSlot slot = event.getCurrentItem().getType().getEquipmentSlot();
+        	if (!slot.toString().equals("BODY") && !isNotNullOrAir(player.getEquipment().getItem(slot))) {
+                PlayerArmorChangeEvent armorEvent = new PlayerArmorChangeEvent(player, slot, new ItemStack(Material.AIR), event.getCurrentItem(), Reason.INVENTORY_ACTION);
                 Bukkit.getPluginManager().callEvent(armorEvent);
                 if (armorEvent.isCancelled()) {
                     event.setResult(Result.DENY);
-                    if (((Player) event.getWhoClicked()).getGameMode() == GameMode.CREATIVE)
-                        ((Player) event.getWhoClicked()).setItemOnCursor(event.getCursor());
-                }
-            }
-            return;
-        }
-        if (event.getClickedInventory().getType() == InventoryType.PLAYER && getTopInventoryType(event) == InventoryType.CRAFTING) {
-            if (event.getSlot() > 35 && event.getSlot() < 40) {
-                if (pickUpActions.contains(event.getAction()) && isNotNullOrAir(event.getCurrentItem())) {
-                    PlayerArmorChangeEvent armorEvent = new PlayerArmorChangeEvent((Player) event.getWhoClicked(), event.getCurrentItem().getType().getEquipmentSlot(), event.getCurrentItem(), new ItemStack(Material.AIR), Reason.INVENTORY_ACTION);
-                    Bukkit.getPluginManager().callEvent(armorEvent);
-                    if (armorEvent.isCancelled())
-                        event.setResult(Result.DENY);
-                    return;
-                }
-                if (placeActions.contains(event.getAction()) && isNotNullOrAir(event.getCursor())) {
-                    PlayerArmorChangeEvent armorEvent = new PlayerArmorChangeEvent((Player) event.getWhoClicked(), event.getCursor().getType().getEquipmentSlot(), new ItemStack(Material.AIR), event.getCursor(), Reason.INVENTORY_ACTION);
-                    Bukkit.getPluginManager().callEvent(armorEvent);
-                    if (armorEvent.isCancelled()) {
-                        event.setResult(Result.DENY);
-                        if (((Player) event.getWhoClicked()).getGameMode() == GameMode.CREATIVE)
-                            ((Player) event.getWhoClicked()).setItemOnCursor(event.getCursor());
+                    if (player.getGameMode() == GameMode.CREATIVE) {
+                    	player.setItemOnCursor(new ItemStack(Material.AIR));
+                    	if (player.getInventory().firstEmpty() != -1)
+                    		player.getInventory().addItem(event.getCursor());
+                    	else
+                    		player.setItemOnCursor(event.getCursor());
                     }
-                    return;
                 }
-            } else if (event.isShiftClick() && isNotNullOrAir(event.getCurrentItem())) {
-                EquipmentSlot slot = event.getCurrentItem().getType().getEquipmentSlot();
-                if (!slot.toString().equals("BODY") && !isNotNullOrAir(((Player) event.getWhoClicked()).getEquipment().getItem(slot))) {
-                    PlayerArmorChangeEvent armorEvent = new PlayerArmorChangeEvent((Player) event.getWhoClicked(), slot, new ItemStack(Material.AIR), event.getCurrentItem(), Reason.INVENTORY_ACTION);
-                    Bukkit.getPluginManager().callEvent(armorEvent);
-                    if (armorEvent.isCancelled())
-                        event.setResult(Result.DENY);
-                    return;
-                }
+                return;
             }
         }
     }
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && event.getHand() != null && !(event.hasBlock() && isInteractable(event.getClickedBlock()))) {
-            Player p = event.getPlayer();
-            ItemStack item = event.getHand() == EquipmentSlot.HAND ? p.getEquipment().getItemInMainHand() : p.getEquipment().getItemInOffHand();
-            if (!isNotNullOrAir(item) || item.getType() == Material.CARVED_PUMPKIN)
-                return;
-            EquipmentSlot slot = item.getType().getEquipmentSlot();
-            if (!armorSlots.contains(slot))
-                return;
-            PlayerArmorChangeEvent armorEvent = new PlayerArmorChangeEvent(p, slot, isNotNullOrAir(p.getEquipment().getItem(slot)) ? p.getEquipment().getItem(slot) : new ItemStack(Material.AIR), item, Reason.RIGHT_CLICK);
-            Bukkit.getPluginManager().callEvent(armorEvent);
-            if (armorEvent.isCancelled())
-                event.setCancelled(true);
-            return;
-        }
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onInventoryDrag(InventoryDragEvent event) {
+    	if (event.getResult() == Result.DENY || !(event.getWhoClicked() instanceof Player))
+    		return;
+    	for (Map.Entry<Integer, ItemStack> entry : event.getNewItems().entrySet()) {
+    		final int slot = entry.getKey();
+    		if (entry.getValue() != null && slot > 4 && slot < 9 && getViewSlotType(event, slot) == SlotType.ARMOR) {
+    			Player player = (Player) event.getWhoClicked();
+    			PlayerArmorChangeEvent armorEvent = new PlayerArmorChangeEvent(player, getEquipmentSlotFromID(slot), new ItemStack(Material.AIR), entry.getValue(), Reason.INVENTORY_ACTION);
+                Bukkit.getPluginManager().callEvent(armorEvent);
+                if (armorEvent.isCancelled()) {
+                    event.setResult(Result.DENY);
+                    return;
+                }
+    		}
+    	}
     }
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+    	if (event.useItemInHand() == Result.DENY 
+    			|| !(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) 
+    			|| event.getHand() == null 
+    			|| (event.hasBlock() && isInteractable(event.getClickedBlock())))
+    		return;
+    	Player player = event.getPlayer();
+    	ItemStack item = event.getHand() == EquipmentSlot.HAND ? player.getEquipment().getItemInMainHand() : player.getEquipment().getItemInOffHand();
+    	if (!isNotNullOrAir(item) || item.getType() == Material.CARVED_PUMPKIN)
+    		return;
+    	EquipmentSlot slot = item.getType().getEquipmentSlot();
+    	if (!armorSlots.contains(slot))
+    		return;
+    	ItemStack current = player.getEquipment().getItem(slot);
+    	PlayerArmorChangeEvent armorEvent = new PlayerArmorChangeEvent(player, slot, current != null ? current : new ItemStack(Material.AIR), item, Reason.RIGHT_CLICK);
+    	Bukkit.getPluginManager().callEvent(armorEvent);
+    	if (armorEvent.isCancelled())
+    		event.setCancelled(true);
+    }
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onBlockDispenseArmor(BlockDispenseArmorEvent event) {
-        if (event.isCancelled() || !(event.getTargetEntity() instanceof Player))
+    	if (!(event.getTargetEntity() instanceof Player))
             return;
         PlayerArmorChangeEvent armorEvent = new PlayerArmorChangeEvent((Player) event.getTargetEntity(), event.getItem().getType().getEquipmentSlot(), new ItemStack(Material.AIR), event.getItem(), Reason.DISPENSER);
         Bukkit.getPluginManager().callEvent(armorEvent);
         if (armorEvent.isCancelled())
             event.setCancelled(true);
     }
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onItemDamage(PlayerItemDamageEvent event) {
-        if (event.isCancelled() || ((Damageable) event.getItem().getItemMeta()).getDamage()+event.getDamage() < event.getItem().getType().getMaxDurability())
+        if (((Damageable) event.getItem().getItemMeta()).getDamage()+event.getDamage() < event.getItem().getType().getMaxDurability())
             return;
         EquipmentSlot slot = event.getItem().getType().getEquipmentSlot();
         if (!armorSlots.contains(slot) || !event.getPlayer().getInventory().getItem(slot).equals(event.getItem()))
@@ -168,5 +230,41 @@ public class PlayerArmorListener implements Listener {
         } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+    private SlotType getViewSlotType(InventoryEvent event, int rawSlot) {
+        try {
+            Object view = event.getView();
+            Method getInventorySlot = view.getClass().getMethod("getSlotType", int.class);
+            getInventorySlot.setAccessible(true);
+            return (SlotType) getInventorySlot.invoke(view, rawSlot);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private EquipmentSlot getEquipmentSlotFromRawID(int slot) {
+    	switch (slot) {
+    	case 36:
+    		return EquipmentSlot.FEET;
+    	case 37:
+    		return EquipmentSlot.LEGS;
+    	case 38:
+    		return EquipmentSlot.CHEST;
+    	case 39:
+    		return EquipmentSlot.HEAD;
+    	}
+    	return null;
+    }
+    private EquipmentSlot getEquipmentSlotFromID(int slot) {
+    	switch (slot) {
+    	case 8:
+    		return EquipmentSlot.FEET;
+    	case 7:
+    		return EquipmentSlot.LEGS;
+    	case 6:
+    		return EquipmentSlot.CHEST;
+    	case 5:
+    		return EquipmentSlot.HEAD;
+    	}
+    	return null;
     }
 }
